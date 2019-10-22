c37e6700a9c0397248ea410a2ecd8136cb389edd
jakefoehner <jake.foehner1@gmail.com>
2019-10-22 19:04:17 -0400

    updates

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    ifstream inputF;
+    ifstream inputF (argv[1], ios::binary);
+    unsigned char byte;
+    unsigned int * mem;
+    int pc = 0;
+    int i = 0;
 
-    inputF.open(argv[1]);
-    if (!inputF) {
-        cout << "Input file could not be opened";
-        exit(1);
+
+    if (inputF.is_open()){
+    	inputF.seekg(0, inputF.end);
+    	int size = inputF.tellg();
+    	inputF.seekg(0, inputF.beg);
+    	mem = new unsigned int [size];
+    	while (!inputF.eof()){
+        	inputF >> byte;
+        	mem[i] = static_cast<unsigned int>(byte);
+			i++;
+    	}
+    	inputF.close();
     }
-
-    inputF.close();
+    else{
+    	cout << "File failed to open." << endl;
+    	exit(1);
+    }
 
     return 0;
 }
