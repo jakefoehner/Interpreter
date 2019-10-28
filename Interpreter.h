70af295592103ce2e1f61fd8a639a6e8800925f9
jakefoehner <jake.foehner1@gmail.com>
2019-10-27 21:15:49 -0400

    updates

--- Interpreter.h
+++ Interpreter.h
@@ -14,6 +14,43 @@ public:
 	Interpreter(unsigned int * input, int s);
 	~Interpreter();
 	void run();
+
+	void cmpe();
+	void cmplt();
+    void cmpgt();
+    void jmp();
+    void jmpc();
+    void call();
+    void ret();
+    void pushc();
+    void pushs();
+    void pushi();
+    void pushf();
+    void pushvc();
+    void pushvs();
+    void pushvi();
+    void pushvf();
+    void popm();
+    void popv();
+    void popa();
+    void peekc();
+    void peeks();
+    void peeki();
+    void peekf();
+    void pokec();
+    void pokes();
+    void pokei();
+    void pokef();
+    void swp();
+    void add();
+    void sub();
+    void mul();
+    void div();
+    void printc();
+    void prints();
+    void printi();
+    void printf();
+    void halt();
 };
 
 #endif
