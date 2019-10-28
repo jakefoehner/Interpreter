b8638e2670370ec9e89a062ba7744e029fda7865
jakefoehner <jake.foehner1@gmail.com>
2019-10-27 21:15:19 -0400

    updates

--- main.cpp
+++ main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "Interpreter.h"
 using namespace std;
 
 int main(int argc, char* argv[]) {
     ifstream inputF (argv[1], ios::binary);
     unsigned char byte;
-    unsigned int * mem;
-    int pc = 0;
-    int i = 0;
+    unsigned int * input;
+    int i = 0, size;
 
-
-    if (inputF.is_open()){
-    	inputF.seekg(0, inputF.end);
-    	int size = inputF.tellg();
-    	inputF.seekg(0, inputF.beg);
-    	mem = new unsigned int [size];
-    	while (!inputF.eof()){
-        	inputF >> byte;
-        	mem[i] = static_cast<unsigned int>(byte);
-			i++;
-    	}
-    	inputF.close();
-    }
-    else{
+    //check if file opened correctly
+    if (!inputF){
     	cout << "File failed to open." << endl;
     	exit(1);
     }
+
+    //fill memory with the input
+	inputF.seekg(0, inputF.end);
+	size = inputF.tellg();
+	inputF.seekg(0, inputF.beg);
+	input = new unsigned int [size];
+	while (!inputF.eof()){
+    	inputF >> byte;
+    	input[i] = static_cast<unsigned int>(byte);
+		i++;
+	}
+	inputF.close();
+
+	//build, and run interpreter
+	Interpreter interp = Interpreter(input, size);
+	interp.run();
 
     return 0;
 }
